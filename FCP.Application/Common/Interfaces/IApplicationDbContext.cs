4edfada672ff261050f61e2d200507ed841d30cb
Pc <Pc@LPC>
2025-12-23 23:48:02 +0100

    git add

---
+++ FCP.Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using FCP.Domain.Entities;
+
+
+namespace FCP.Application.Common.Interfaces
+{
+    public interface IApplicationDbContext
+    {
+        DbSet<Course> Courses { get; }
+        DbSet<CourseItem> CourseItems { get; }
+        DbSet<Provider> Providers { get; }
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
